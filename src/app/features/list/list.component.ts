397bde98094a1512152b4a02dc12e713f89a5a2c
matheusJRM <matheusjrmoura@gmail.com>
2024-05-16 20:56:21 -0300

    feat: adicionado funcionalidade de deleção de produtos na listagem

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,6 +4,38 @@ import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { filter } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  template: `<h2 mat-dialog-title>Deletar Produto</h2>
+    <mat-dialog-content>
+      Tem certeza que quer deletar esse produto?
+    </mat-dialog-content>
+    <mat-dialog-actions align="end">
+      <button mat-flat-button (click)="onClose()">Não</button>
+      <button mat-flat-button (click)="onConfirm()" cdkFocusInitial>Sim</button>
+    </mat-dialog-actions>`,
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class DialogAnimationsExampleDialog {
+  dialogRef = inject(MatDialogRef);
+
+  onClose() {
+    this.dialogRef.close(false);
+  }
+
+  onConfirm() {
+    this.dialogRef.close(true);
+  }
+}
 
 @Component({
   selector: 'app-list',
@@ -14,10 +46,10 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class ListComponent {
   products: Product[] = [];
-
   productsService = inject(ProductsService);
-
   router = inject(Router);
+  dialog = inject(MatDialog);
+  matSnackBar = inject(MatSnackBar);
 
   ngOnInit() {
     this.productsService.getAll().subscribe((products) => {
@@ -27,5 +59,24 @@ export class ListComponent {
 
   onEdit(id: string) {
     this.router.navigate(['/edit-product', id]);
+  }
+
+  onDelete(id: string) {
+    this.dialog
+      .open(DialogAnimationsExampleDialog, {
+        data: { id },
+      })
+      .afterClosed()
+      .pipe(filter((answer: boolean) => answer === true))
+      .subscribe(() => {
+        this.productsService.delete(id).subscribe(() => {
+          this.matSnackBar.open('Produto deletado com sucesso!', 'Ok', {
+            panelClass: ['app-notification-success'],
+          });
+          this.productsService.getAll().subscribe((products) => {
+            this.products = products;
+          });
+        });
+      });
   }
 }
